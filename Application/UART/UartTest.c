0a915c71e411edbdce386e0d78a4e11d334f3d2f
wang12zhe <635432187@qq.com>
2020-09-20 21:29:50 +0800

    use two pthread for to uart send and receive

--- Application/UART/UartTest.c
+++ Application/UART/UartTest.c
@@ -22,65 +22,98 @@
 #include<termios.h>    /*PPSIX 终端控制定义*/    
 #include<errno.h>      /*错误号定义*/    
 #include<string.h>
+#include <pthread.h>
  
- 
+struct Command{
+    unsigned char Index;
+    unsigned char Description[30];
+    void (*fp_Action)(int fd, unsigned char send_buf[],unsigned int len);
+};
+
+void Action(int fd, unsigned char send_buf[],unsigned int len){
+    len = UART0_Send(fd,send_buf,len);
+}
+
+struct Command Cmd[] ={
+{'A',"111111111111",Action},
+{'B',"BBBBBBBBBBBB",Action},
+{'C',"CCCCCCCCCCCC",Action},
+};
+
+void SendData(void *arg){
+
+   char ReadStr[50];
+   unsigned int ComdLen = sizeof(Cmd)/sizeof(Cmd[0]);
+   unsigned int i=0,j=0;
+   int fd = *(int *)arg;
+    while(1){
+        fgets(ReadStr,50,stdin);
+        j =0;
+        if(strlen(ReadStr) > 0){
+            while(ReadStr[j]){
+                for(i =0;i < ComdLen;i++){
+                    if (ReadStr[j] == Cmd[i].Index){
+                        Cmd[i].fp_Action(fd,Cmd[i].Description,strlen(Cmd[i].Description));  
+                        break;  
+                    }
+                }
+                j ++;
+            }
+        }else{
+            sleep(1);
+        }
+    }
+}
+void Receive (void *arg){
+    char len=0;
+    char rcv_buf[256];
+    int fd = *(int *)arg;
+    while (1) {  //循环读取数据
+        memset(rcv_buf,0,256);      
+        len = UART0_Recv(fd, rcv_buf,sizeof(rcv_buf)); 
+        if(len > 0){    
+            rcv_buf[len] = '\0';    
+            printf("receive data is %s\n",rcv_buf); 
+        }    
+        else{     
+        }                   
+    }
+}
+//start gdbserver on linux gdbserver localhost:1234 /home/a123/UART/usart /dev/ttyS0
 int main(int argc, char **argv)    
 {
 	int fd = -1;           //文件描述符，先定义一个与程序无关的值，防止fd为任意值导致程序出bug    
     int err;               //返回调用函数的状态    
     int len;                            
     int i;    
-    char rcv_buf[256];             
-    char send_buf[256];
-    if(argc != 3)    
-    {    
-        printf("Usage: %s /dev/ttySn 0      #(send data)\n",argv[0]);
-        printf("Usage: %s /dev/ttySn 1      #1(receive data)\n",argv[0]);
-        printf("open failure : %s\n", strerror(errno));
-    
+    pthread_t	ntid1,ntid2;
+             
+    if(argc != 2)    
+    {  
+        printf("=========================\n");  
+        printf("Usage: %s /dev/ttyS0     \n",argv[0]);
+        printf("=========================\n");
         return FALSE;    
     }    
      fd = UART0_Open(fd,argv[1]); //打开串口，返回文件描述符   
-     // fd=open("dev/ttyS1", O_RDWR);
-    //printf("fd= \n",fd);
-     do  
-    {    
+     printf("iiiiiiiiiii-%d",fd);
+     do{    
         err = UART0_Init(fd,115200,0,8,1,'N');    
         printf("Set Port Exactly!\n"); 
         sleep(1);   
     }while(FALSE == err || FALSE == fd);    
-       
-    if(0 == strcmp(argv[2],"0"))    //开发板向pc发送数据的模式
-    {   
-        fgets(send_buf,256,stdin);   //输入内容，最大不超过40字节，fgets能吸收回车符，这样pc收到的数据就能自动换行     
-        for(i = 0;i < 10;i++)    
-        {    
-            len = UART0_Send(fd,send_buf,40);    
-            if(len > 0)    
-                printf(" %d time send %d data successful\n",i,len);    
-            else    
-                printf("send data failed!\n");    
-                              
-            sleep(1);    
-        }    
-        UART0_Close(fd);                 
-    }    
-    else                            //开发板收到pc发送的数据的模式                 
-    {                                          
-        while (1) //循环读取数据    
-        {   
-            len = UART0_Recv(fd, rcv_buf,sizeof(rcv_buf));    
-            if(len > 0)    
-            {    
-                rcv_buf[len] = '\0';    
-                printf("receive data is %s\n",rcv_buf);    
-            }    
-            else    
-            {    
-                printf("cannot receive data\n");    
-            }    
-            sleep(1);    
-        }                
-        UART0_Close(fd);     
-    }    
+
+
+	err = pthread_create(&ntid1, NULL, (void *)SendData, (&fd));
+	if (err != 0){
+        perror("can't create thread");     
+    }
+
+    err = pthread_create(&ntid2, NULL, (void *)Receive, (&fd));
+	if (err != 0){
+        perror("can't create thread");     
+    }
+
+pthread_join(ntid1,NULL);
+pthread_join(ntid2,NULL);
 } 
